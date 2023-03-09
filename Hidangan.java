a3ddc363dcb6463027ca4d057744726d1544660b
Raihancuy <127366069+Raihancuy@users.noreply.github.com>
2023-03-09 10:07:11 +0700

    Add files via upload

---
+++ Hidangan.java
@@ -0,0 +1,17 @@
+public class Hidangan {
+
+    protected String namaHidangan;
+
+    public String getNamaHidangan(){
+        return namaHidangan;
+    }
+
+    public String setNamaHidangan(String namaHidangan){
+        this.namaHidangan = namaHidangan;
+        return namaHidangan;
+    }
+
+    public String disantap(){
+        return "Makanan Dihidangkan";
+    }
+}
